36a5005aa8bba96bfeb3da047d4db29a4a1714d7
kaylamyers <155663787+kaylamyers@users.noreply.github.com>
2024-08-04 20:45:29 -0500

    Update braille.py

--- braille.py
+++ braille.py
@@ -1,12 +1,14 @@
 def main():
+    # user input lyrics to song
     lyrics = input("Enter the lyrics to be translated to Braille: ").lower()
     print(convert_lyrics(lyrics))
 
+    # user input musical notes to song
     notes = input("Enter the musical notes to be translated to Braille: ").lower()
-    ind_note = notes.split(" ")
-    element = []
+    ind_note = notes.split(" ")  # splitting notes at the space
+    element = [] 
     for note in ind_note:
-        element.append(note)
+        element.append(note)  # adding note into element space
     print(convert_notes(element))
 
 
@@ -26,6 +28,7 @@ def convert_lyrics(lyrics):
     ' ': ' ', '.': '⠲', ',': '⠂', '?': '⠦', '!': '⠖', '-': '⠤',
     }
 
+    # conversion of alphabet to braille letters
     lyrics = "".join(braille_letters.get(letter, letter) for letter in lyrics)
     return lyrics
     
@@ -66,11 +69,13 @@ def convert_notes(notes):
 
     braille_translate = ''
     for cell in notes:
+        # looking up braille equivalent & using cell itself if not found
         braille_char = braille_music.get(cell, cell)
+        # adding braille char to end of translation string w space for readability
         braille_translate += braille_char + ' '
     
+    # trimming string to return
     return braille_translate.strip()
 
 
 main()
-
